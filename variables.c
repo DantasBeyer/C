a640be9d5ad8331010d8d96a2b570bda285085e6
Joao Victor Dantas Beyer <joaovictordantasbeyer@MBP-von-Joao.fritz.box>
2023-06-12 20:02:00 +0200

    updated

---
+++ variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+// variables = Allocated space in memory to store a value.
+//             We need to declare what type of data we are storing.
+
+int main(){
+
+    int x;       // declaration
+    x = 123;      // initialization
+    int y = 321; // declaration + initialization
+
+    int age = 32;        // integer
+    float gpa = 2.05;     // floating point number
+    char grade = 'C';     // single character
+    char name[] = "Joao"; // array of characters
+
+    return 0;
+}
