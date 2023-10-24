7fb445791ac477a90a5fd83921fe3bf1007d66b9
João Beyer <100128679+DantasBeyer@users.noreply.github.com>
2023-10-24 13:35:34 +0200

    Schleifen

---
+++ Schleifen/for/for_1.c
@@ -0,0 +1,10 @@
+// Hier wird eine einfache Zählschleife demosntriert.
+
+#include <stdio.h>
+
+int main (void){
+    for(int cnt = 1; cnt <= 5; cnt++ ){
+        printf("%d. Schleifendurchlauf\n", cnt);
+    }
+    return 0;
+}
