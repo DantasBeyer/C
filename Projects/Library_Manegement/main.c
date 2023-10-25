b3c71f6cdac0b7b345a69078139f1040a1a58238
João Beyer <100128679+DantasBeyer@users.noreply.github.com>
2023-10-25 10:31:06 +0200

    upload projects

---
+++ Projects/Library_Manegement/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct library
+{
+    char book_name[100];
+    char author_name[100];
+    float cost;
+    int no_of_pages;
+};
+
+int main()
+{
+    struct library lib[100];
+    char book_name[100];
+
+    int i, j, count;
+    i = 0;
+    j = 0;
+    count = 0;
+
+    while (j != 6)
+    {
+        switch (j)
+        {
+        /*
+        case:
+            break;
+
+        default:
+            break; */
+        }
+    }
+}
